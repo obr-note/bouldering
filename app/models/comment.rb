a6291548251e8ba4150cb569ed42092250e6c385
obr-note <k33mdukb9n@yahoo.co.jp>
2019-12-22 10:15:22 +0900

    webpacker:install

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
+  validates :evaluation, presence: true, numericality: true
+  validates :comment, presence: true, length: { in: 1..50 }
+
   belongs_to :gym
   belongs_to :user
+
+  has_many :nices
+  has_many :nice_users, through: :nices, source: :user
 end
--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,5 +1,11 @@
 class Gym < ApplicationRecord
+  validates :address, presence: true, length: { in: 1..50 }
+  validates :work_time, presence: true, length: { in: 1..50 }
+  validates :price, presence: true, length: { in: 1..50 }
+  validates :gym_url, presence: true, length: { in: 1..50 }
+
+
   belongs_to :area
-  belongs_to :created_user
-  belongs_to :updated_user
+  belongs_to :created_user, class_name: 'User'
+  belongs_to :updated_user, class_name: 'User'
 end
