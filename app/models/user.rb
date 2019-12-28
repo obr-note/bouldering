36fb9c93dcfd6b212972c60d3cf8ac4796f421f6
obr-note <k33mdukb9n@yahoo.co.jp>
2019-12-28 21:54:48 +0900

    add comment in Gemfile,views for myself & create signup page & install bcrypt for has_secure_password method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { in: 3..10 }, uniqueness: true
     has_secure_password
 
-    has_many: comments
-    has_many: gyms
-    has_many: nices
-    has_many: nice_comments, through: :nices, source: :comment
+    has_many :comments
+    has_many :gyms
+    has_many :nices
+    has_many :nice_comments, through: :nices, source: :comment
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root to: "home#index" #トップページ
+
+  resources :users, only: [:new, :create] #ユーザー登録
 end
